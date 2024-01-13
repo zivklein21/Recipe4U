f07a0da737860009ad1486c59c9dfceeeb46401d
edosb111 <edo.sabri123@gmail.com>
2024-01-13 15:27:59 +0200

    Initial Commit

---
+++ app/src/main/java/com/cc/recipe4u/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cc.recipe4u
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
